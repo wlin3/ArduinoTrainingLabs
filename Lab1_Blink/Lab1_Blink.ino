07ed7a754a8ee3b4c79bdaa2cb5b368c552d069d
William Lin <02william.lin@gulllakecs.org>
2019-11-14 14:04:46 -0500

    initial DigitalRead() & the Serial Port

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,24 +1,20 @@
 
 /*Programmer: Willliam Lin
  * Date: 11.6.19
- * Program: Blink
+ * Program: digitalRead & the Serial Port
  * 
- * This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop
+ *Reads a digital input on pin2, prints
  */
 
-// itialize our led to Pin 13
-int led = 13;
+
 
 void setup() {
-  // Using the Function pinMode to call the vairable led and provide OUTPUT
-  pinMode(led, OUTPUT);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led, HIGH);
-  delay(500);
-  digitalWrite(led, LOW);
-  delay(2000);
+
+
   }
   
