62d247d2659e2e9f6cdf144d6a8681140e6d0f4c
William Lin <02william.lin@gulllakecs.org>
2019-11-14 13:41:57 -0500

    Adjusted LED Timings

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -17,7 +17,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(led, LOW);
-  delay(1000);
+  delay(2000);
   }
+  
