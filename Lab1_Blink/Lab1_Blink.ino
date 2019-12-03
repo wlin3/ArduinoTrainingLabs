5da39c2440c66049b66976c2dcbc37f4d5d324e2
William Lin <02william.lin@gulllakecs.org>
2019-12-03 14:08:16 -0500

    Initial Potentiometer Program

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,46 +1,17 @@
 
 /* Programmer: Willliam Lin
    Date: 11.6.19
-   Program: Blink
+   Program: Potentiometer
 
-   This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop
+   AnalogReadSerial
+   Reads an analog input on pin A0, prints to the serial monitor
 
-   Merged on 12.2.19
 
-   Programmer: Willliam Lin
-   Date: 11.6.19
-   Program: digitalRead & the Serial Port
-
-   Reads a digital input on pin2, prints
 */
 
-
-// itialize our led to Pin 13
-int led = 13;
-
-//digital pin 2 has a push button attached to it
-int pushButton = 2;
-
 void setup() {
-  // Using the Function pinMode to call the vairable led and provide OUTPUT
-  pinMode(led, OUTPUT);
-  //initializes serial communication at 9600 bits per second
-  Serial.begin(9600);
-  //make the pushButton pin an input
-  pinMode(pushButton, INPUT);
+
 }
 
 void loop() {
-  // read the input pin
-  int buttonState = digitalRead(pushButton);
-  //print out the state of the button
-  Serial.println(buttonState);
-  // if the buttonState is True/Pushed by user, the LED will blink
-  if (buttonState == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
-  }
-  delay(1); //delay in between readings for stability
 }
